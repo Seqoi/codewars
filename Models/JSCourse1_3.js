919c81dc703822018460ce1e4a9381b070b8a0b7
Botan3301 <darikin123497@mail.ru>
2023-08-17 10:45:14 +0300

    something new

---
+++ Models/JSCourse1_3.js
@@ -0,0 +1,21 @@
+//1-st day
+alert("Привет JavaScript, я и до тебя добрался!");
+//2-nd day
+let name = 'Jhon';
+let admin;
+admin = name;
+alert(admin);
+
+const currentPlanet = 'Earth';
+let currentUser = admin;
+
+const BIRTHDAY = '14.05.1997';//Здесь заглавными так как дата известна
+//с самого начала
+// const age = someCode(BIRTHDAY);//А тут мы узнаем возраст только после
+//исполнения кода
+
+//3-rd day
+//the page that take the name of user and alert him
+let userName = prompt("What is u're name?","Name");
+alert(userName);
+
---
+++ Models/JSCourse4_8.js
@@ -0,0 +1,13 @@
+//We ended with function's of alert prompt and confirm
+/*alert - output one message
+  prompt - output one message and take strinng on enter
+  saving him on the variable*/
+/* confirm - output the answer and save the answer in boolean variable*/
+//We continue with swapping the types of data 
+alert("" + 1 + 0);
+alert("4px" - 2);
+alert("  9  " - 5);
+alert("  -9  " - 5);
+alert(null + 1);
+alert(undefined + 1);
+alert(" \t 5 \n \n" + 2);
